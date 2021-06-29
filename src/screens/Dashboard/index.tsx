dbb8079db1eb13b5d41d337b7e54f0f7becee99e
Bruno Martins Crocomo <bruno.crocomo@gmail.com>
2021-06-29 14:22:52 -0300

    Add Dashboard bare bones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboards</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/8473672?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Bruno</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
